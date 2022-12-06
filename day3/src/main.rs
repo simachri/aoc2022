ae7019be954a3814f3311d07950857f49a604977
simachri <marvin.christe@posteo.de>
2022-12-06 16:22:22 +0100

    Day 3, part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,9 +1,13 @@
+#![feature(iter_next_chunk)]
+
 fn main() {
     let input = include_str!("../input.txt");
 
     let result_day_1 = calculate_sum_day_1(input);
+    let result_day_2 = calculate_sum_day_2(input);
 
     println!("Result of day 1: {}", result_day_1);
+    println!("Result of day 2: {}", result_day_2);
 }
 
 fn calculate_sum_day_1(input: &str) -> u32 {
@@ -12,7 +16,6 @@ fn calculate_sum_day_1(input: &str) -> u32 {
 
         let items_per_compartment = rucksack.len() / 2;
         for item_left in rucksack.chars().take(items_per_compartment) {
-
             for item_right in rucksack.chars().skip(items_per_compartment) {
                 if item_left == item_right {
                     double_item = item_left;
@@ -32,6 +35,45 @@ fn calculate_sum_day_1(input: &str) -> u32 {
     });
 }
 
+fn calculate_sum_day_2(input: &str) -> u32 {
+    let mut sum = 0;
+    let mut lines = input.lines();
+
+    loop {
+        let group = match lines.next_chunk::<3>() {
+            Ok(v) => v,
+            Err(_) => break, // no remaining lines; input data can be divided by three
+        };
+
+        let mut badge_item = '1';
+
+        for item_elf_1 in group[0].chars() {
+            for item_elf_2 in group[1].chars() {
+                if item_elf_1 == item_elf_2 {
+                    for item_elf_3 in group[2].chars() {
+                        if item_elf_1 == item_elf_3 {
+                            badge_item = item_elf_1;
+                            break;
+                        }
+                    }
+                }
+
+                if !badge_item.is_numeric() {
+                    break;
+                }
+            }
+
+            if !badge_item.is_numeric() {
+                break;
+            }
+        }
+        assert!(!badge_item.is_numeric());
+
+        sum += get_priority(badge_item);
+    }
+
+    return sum;
+}
 fn get_priority(char: char) -> u32 {
     if char.is_lowercase() {
         // 'a' starts at unicode scalar value 96.
@@ -43,7 +85,6 @@ fn get_priority(char: char) -> u32 {
         return char as u32 - 38;
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -61,6 +102,22 @@ CrZsJsPPZsGzwwsLwLmpwMDw
 
         let got = calculate_sum_day_1(input);
         let want = 157;
+
+        assert_eq!(want, got);
+    }
+
+    #[test]
+    fn test_day_2() {
+        let input = r"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+";
+
+        let got = calculate_sum_day_2(input);
+        let want = 70;
 
         assert_eq!(want, got);
     }
