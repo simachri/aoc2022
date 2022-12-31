7f89822ee9cba16df4596b6373db1f550b11331e
simachri <marvin.christe@posteo.de>
2022-12-31 14:07:12 +0100

    day 8, part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -7,8 +7,112 @@ fn main() {
     let input = include_str!("../input.txt");
 
     let result_part1 = calculate_visible_trees(input);
+    let result_part2 = calculate_scenic_score(input);
 
     println!("Result part 1: {}", result_part1);
+    println!("Result part 2: {}", result_part2);
+}
+
+fn calculate_scenic_score(input: &str) -> u32 {
+    let tree_map = init_tree_map(input);
+    let scenic_score = calculate_scenic_score_for_each_tree(&tree_map);
+
+    scenic_score
+}
+
+fn calculate_scenic_score_for_each_tree(tree_map: &Vec<Vec<Tree>>) -> u32 {
+    let mut max_scenic_score = 0;
+
+    // the outer trees are irrelevant
+    for i in 1..tree_map.len() - 1 {
+        for j in 1..tree_map[i].len() - 1 {
+            let scenic_score = visible_trees_left(&tree_map, tree_map[i][j].size, i, j)
+                * visible_trees_right(&tree_map, tree_map[i][j].size, i, j)
+                * visible_trees_up(&tree_map, tree_map[i][j].size, i, j)
+                * visible_trees_down(&tree_map, tree_map[i][j].size, i, j);
+
+            if scenic_score > max_scenic_score {
+                max_scenic_score = scenic_score;
+            }
+        }
+    }
+
+    max_scenic_score
+}
+
+fn visible_trees_down(
+    tree_map: &Vec<Vec<Tree>>,
+    tree_size: u8,
+    row_idx: usize,
+    col_idx: usize,
+) -> u32 {
+    let mut visible_trees = 1; // at least one tree is visible
+
+    for i in row_idx + 1..tree_map.len() - 1 {
+        if tree_map[i][col_idx].size < tree_size {
+            visible_trees += 1;
+        } else {
+            break;
+        }
+    }
+
+    visible_trees
+}
+fn visible_trees_up(
+    tree_map: &Vec<Vec<Tree>>,
+    tree_size: u8,
+    row_idx: usize,
+    col_idx: usize,
+) -> u32 {
+    let mut visible_trees = 1; // at least one tree is visible
+
+    for i in (1..row_idx).rev() {
+        if tree_map[i][col_idx].size < tree_size {
+            visible_trees += 1;
+        } else {
+            break;
+        }
+    }
+
+    visible_trees
+}
+
+fn visible_trees_right(
+    tree_map: &Vec<Vec<Tree>>,
+    tree_size: u8,
+    row_idx: usize,
+    col_idx: usize,
+) -> u32 {
+    let mut visible_trees = 1; // at least one tree is visible
+
+    for i in col_idx + 1..tree_map[row_idx].len() - 1 {
+        if tree_map[row_idx][i].size < tree_size {
+            visible_trees += 1;
+        } else {
+            break;
+        }
+    }
+
+    visible_trees
+}
+
+fn visible_trees_left(
+    tree_map: &Vec<Vec<Tree>>,
+    tree_size: u8,
+    row_idx: usize,
+    col_idx: usize,
+) -> u32 {
+    let mut visible_trees = 1; // at least one tree is visible
+
+    for i in (1..col_idx).rev() {
+        if tree_map[row_idx][i].size < tree_size {
+            visible_trees += 1;
+        } else {
+            break;
+        }
+    }
+
+    visible_trees
 }
 
 fn calculate_visible_trees(input: &str) -> u32 {
@@ -171,5 +275,11 @@ mod tests {
     fn test_part1() {
         let input = include_str!("../test.txt");
         assert_eq!(21, calculate_visible_trees(input));
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = include_str!("../test.txt");
+        assert_eq!(8, calculate_scenic_score(input));
     }
 }
